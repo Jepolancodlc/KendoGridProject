6bb8feacb3241cd9cc997e9eda883cf6b3a19017
John Polanco <76429837+Jepolancodlc@users.noreply.github.com>
2021-06-01 13:01:07 +0200

    Ajuste de modelo

--- src/app/model-puestos.ts
+++ src/app/model-puestos.ts
@@ -1,7 +1,27 @@
-export class Puestos {
+export class Puesto {
   public codigo: number;
   public codOficial: number;
   public puestoTipo: string;
   public denominacion: string;
-
+  public tipoVinculo: string;
+  public catalogo: string;
+  public adscripcion: string;
+  public g1: string;
+  public g2: string;
+  public escala: string;
+  public cuerpo: string;
+  public formaProvision: string;
+  public grupoImporte: number;
+  public complementoDestinoId: number;
+  public complementoDestinoImporte: number;
+  public complementoEspecificoId: number;
+  public complementoEspecificoImporte: number;
+  public dotaciones: number;
+  public ocupantes: string;
+  public vacantes: string;
+  public dotado: boolean;
+  public singularizado: boolean;
+  public reservadoNacionales: boolean;
+  public disponibilidadPlena: boolean;
+  public personaDeConfianza: boolean;
 }
